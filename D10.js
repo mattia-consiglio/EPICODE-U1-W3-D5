ff71a6a2d05b54fe167960e377bc4ed19d12dd74
Mattia Consiglio <mattiaconsiglio96@gmail.com>
2023-12-09 00:49:23 +0100

    Added HTML elements to prevent errors in the console

--- D10.js
+++ D10.js
@@ -541,7 +541,7 @@ const halfTree = number => {
 	return tree
 }
 
-// console.log(halfTree(10))
+console.log(halfTree(10))
 
 /* ESERCIZIO 28
   Crea una funzione chiamata "tree" che riceve un numero come parametro e costruisce un albero di "*" (asterischi) dell'altezza fornita.
@@ -575,7 +575,7 @@ const tree = number => {
 	return treeArr.join('\n')
 }
 
-console.log(tree(5))
+console.log(tree(10))
 /* ESERCIZIO 29
   Crea una funzione chiamata "isItPrime" che riceve un numero come parametro e ritorna true se il numero fornito è un numero primo.
 */
